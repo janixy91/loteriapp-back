d0ab3f1e0c72001e6c6c175213f23cee5bb4ec25
jany <janire.ng@gmail.com>
2019-12-06 14:45:00 +0100

    deploy

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
