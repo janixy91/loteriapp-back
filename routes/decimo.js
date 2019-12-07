5c5e25ca8a35df93ef1006f02eeb3e5eb254da3b
jany <janire.ng@gmail.com>
2019-12-07 18:10:18 +0100

    comprobar todos los decimos

--- routes/decimo.js
+++ routes/decimo.js
@@ -3,40 +3,115 @@ var router = express.Router();
 var request = require('request-promise-native');
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', async function (req, res, next) {
+
+  if (inTime(req.query.type)) {
+
+    const response = await checkOne(options, req.query.type);
+    const isYear = new Date(response.timestamp).getFullYear() === 2019 || new Date(1545498714 * 1000).getFullYear() === 2020;
+    const status = getStatus(response.status, isYear, response.premio);
+    data = getData(response, status, isYear);
+
+  } else {
+    data = {
+      error: 0,
+      amount: 0,
+      status: 'pending',
+      message: {
+        text: 'El sorteo no ha empezado',
+        title: '¡Una cosa!'
+      }
+    }
+  }
+  res.json(data);
+});
+
+router.post('/', async function (req, res, next) {
+  const decimos = req.body.data;
+  if (inTime(req.query.type)) {
+    let total = 0;
+    let statusElPais;
+    let isYear;
+    for (let decimo of decimos) {
+      const response = await checkOne(decimo.number, req.query.type);
+      statusElPais = response.status
+      isYear = new Date(response.timestamp).getFullYear() === 2019 || new Date(1545498714 * 1000).getFullYear() === 2020;
+      const status = getStatus(response.status, isYear, response.premio);
+      if (status === 'win') {
+        total += response.premio;
+      }
+      decimo.status = status;
+      decimo.quantity = response.premio;
+    }
+    let message;
+    if (!isYear || statusElPais === 0) {
+      message = {
+        text: 'El sorteo no ha empezado',
+        title: '¡Una cosa!'
+      }
+    } else
+      if (total > 0 && statusElPais !== 4) {
+        message = {
+          text: `¡Es posible que hayas GANADO ${total}€!, pero los datos aún no son definitivos`,
+          title: '¡Posible Premio!'
+        }
+      } else if (total > 0 && statusElPais === 4) {
+        message = {
+          text: `¡Según los datos proporcionados por EL PAIS has GANADO ${total}€!`,
+          title: '¡Premio!'
+        }
+      } else if (total === 0 && statusElPais !== 4) {
+        message = {
+          text: `Es posible que no hayas sido premiado, pero los datos aún no son definitivos`,
+          title: 'Lo sentimos'
+        }
+      } else if (total === 0 && statusElPais === 4) {
+        message = {
+          text: `Segun los datos proporcionados por EL PAIS tus décimos no han sido premiados`,
+          title: 'Lo sentimos'
+        }
+      }
+    data = {
+      error: 0,
+      quantity: total,
+      message: message,
+      decimos: decimos
+    }
+  } else {
+    data = {
+      error: 0,
+      quantity: 0,
+      status: 'pending',
+      message: {
+        text: 'El sorteo no ha empezado',
+        title: '¡Una cosa!'
+      },
+      decimos: []
+
+    }
+  }
+
+  res.json(data);
+
+});
+
+const inTime = (type) => {
+  return ((type === 'navidad' && new Date() > new Date("2019/12/22")) || (type === 'nino' && new Date() > new Date("2020/01/6")))
+}
+
+const checkOne = (number, type) => {
   var options = {
     method: 'get',
     json: true,
-    url: req.query.type === 'nino' ? `http://api.elpais.com/ws/LoteriaNinoPremiados?n=${req.query.code}` : `http://api.elpais.com/ws/LoteriaNavidadPremiados?n=${req.query.code}`,
+    url: type === 'nino' ? `http://api.elpais.com/ws/LoteriaNinoPremiados?n=${number}` : `http://api.elpais.com/ws/LoteriaNavidadPremiados?n=${number}`,
   }
-  request(options).then((responseText) => {
-    let data;
-    const response = JSON.parse(responseText.replace('busqueda=', ''))
-    const isYear = new Date(response.timestamp).getFullYear() === 2019 || new Date(1545498714 * 1000).getFullYear() === 2020;
-    const status = getStatus(response.status, isYear, response.premio);
 
-    if (response.error === 0) {
-      if (req.query.type === 'navidad' && new Date() > new Date("2019/12/22")) {
-        data = getData(response, status, isYear);
-      } else if (req.query.type === 'nino' && new Date() > new Date("2020/01/6")) {
-        data = getData(response, status, isYear);
-      } else {
-        data = {
-          error: 0,
-          amount: 0,
-          status: 'pending',
-          message: {
-            text: 'El sorteo no ha empezado',
-            title: '¡Una cosa!'
-          }
-        }
-      }
-    }
-    res.json(data);
-  }).catch((error) => {
-    res.json(error);
+  return new Promise(resolve => {
+    request(options).then((responseText) => {
+      resolve(JSON.parse(responseText.replace('busqueda=', '')))
+    })
   })
-});
+}
 
 const getData = (response, status, isYear) => {
   return {
@@ -51,7 +126,7 @@ const getData = (response, status, isYear) => {
 const getStatus = (resStatus, isYear, premio) => {
   let status;
 
-  if (!isYear || resStatus === 0) {
+  if (!isYear && resStatus === 0) {
     status = 'pending'
   } else if (premio > 0) {
     status = 'win'
@@ -75,12 +150,12 @@ const getMessage = (status, isYear, premio, responseStatus) => {
   } else if (status === 'win') {
     if (responseStatus !== 4) {
       message = {
-        text: `¡Es posible que hayas GANADO ${premio}€ !, pero los datos aún no son definitivos`,
+        text: `¡Es posible que hayas GANADO ${premio}€!, pero los datos aún no son definitivos`,
         title: '¡Posible Premio!'
       }
     } else {
       message = {
-        text: `¡ Segun los datos proporcionados por elpais.com has GANADO ${premio}€ !`,
+        text: `¡ Segun los datos proporcionados por ELPAIS has GANADO ${premio}€!`,
         title: '¡Premio!'
       }
     }
@@ -92,7 +167,7 @@ const getMessage = (status, isYear, premio, responseStatus) => {
       }
     } else {
       message = {
-        text: `Segun los datos proporcionados por elpais.com el decimo no ha sido premiado.`,
+        text: `Segun los datos proporcionados por ELPAIS el decimo no ha sido premiado.`,
         title: 'Lo sentimos'
       }
     }
@@ -102,3 +177,4 @@ const getMessage = (status, isYear, premio, responseStatus) => {
 }
 
 module.exports = router;
+
