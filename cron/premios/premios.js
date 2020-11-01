cc0497f472f639529cdf0420f2d4c0f13461ccc1
jany <janire.ng@gmail.com>
2020-11-01 19:21:28 +0100

    prueba

--- cron/premios/premios.js
+++ cron/premios/premios.js
@@ -1,4 +1,4 @@
-var cron = require('node-cron');
+var cron = require("node-cron");
 console.log(process.env.cron);
 var http = require("http");
 var checkPremios = require("./checkPremios");
@@ -6,20 +6,29 @@ var checkPremios = require("./checkPremios");
 const isCron = process.env.cron;
 console.log(process.env.environment);
 
+console.log("holaisCronisCronisCronisCronisCron", isCron);
 if (!isCron || isCron !== "no") {
-  cron.schedule('*/20 * 22,6 Jan,Dec *', () => {
-    console.log('no duermas');
-    http.get("http://loteriapp-1.herokuapp.com/");
-  });
+  // cron.schedule('*/20 * 22,6 Jan,Dec *', () => {
+  //   console.log('no duermas');
+  //   http.get("http://loteriapp-1.herokuapp.com/");
+  // });
 
-  cron.schedule('* * 22,6 Jan,Dec *', () => {
-    if (new Date().getMonth() === 11) {
-      console.log('go navidad');
-      checkPremios("navidad")
+  cron.schedule(
+    "*/20 10,11,12,13,14,15,16,17,18,19,20,21,22 * Jan,Dec *",
+    () => {
+      console.log("no duermas");
+      http.get("http://loteriapp-1.herokuapp.com/");
+    }
+  );
+
+  // cron.schedule('* * 22,6 Jan,Dec *', () => {
+  cron.schedule("* * 1,6 Nov,Dec *", () => {
+    if (new Date().getMonth() === 10) {
+      console.log("go navidad");
+      checkPremios("navidad");
     } else {
-      console.log('go nino');
-      checkPremios("nino")
+      console.log("go nino");
+      checkPremios("nino");
     }
   });
 }
-
