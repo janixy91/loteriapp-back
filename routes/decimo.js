3ff87cdf830736170475fd3008df7066a6b5c53b
jany <janire.ng@gmail.com>
2022-11-05 13:22:03 +0100

    YEARNAVIDAD2

--- routes/decimo.js
+++ routes/decimo.js
@@ -66,7 +66,7 @@ const inTime = (type) => {
 };
 
 const checkOne = (number, type) => {
-  console.log(YEARNAVIDAD, "YEARNAVIDAD1");
+  console.log(YEARNAVIDAD, "YEARNAVIDAD2");
   var options = {
     method: "get",
     json: true,
