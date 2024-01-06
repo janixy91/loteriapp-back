fb432c64f8527bbb8b43b673457fdba7361f4332
jany <janire.ng@gmail.com>
2024-01-06 12:21:54 +0100

    a

--- routes/decimo.js
+++ routes/decimo.js
@@ -28,46 +28,46 @@ router.get("/", async function (req, res, next) {
 router.post("/", async function (req, res, next) {
   const decimos = req.body.data;
   console.log(req.query.type, "req.query.type");
-  if (inTime(req.query.type)) {
-    let total = 0;
-    let status;
-    let statusElPais;
-    for (let decimo of decimos) {
-      let response = null;
-      try {
-        response = await checkOne(decimo.number, req.query.type);
-        if (response) {
-          statusElPais = response.status;
-          status = getStatus(statusElPais, response.total, response.timestamp);
-          const premio = getQuantityByAmount(decimo.amount, response.total);
-          if (status === "pending") {
-            break;
-          } else if (status === "win") {
-            total += premio;
-          }
-          decimo.status = status;
-          decimo.statusElPais = statusElPais;
-          decimo.quantity = premio;
-          if (status !== "pending") {
-            status = total > 0 ? "win" : "lost";
-          }
-          const message = getMessage(status, total, statusElPais);
-          data = {
-            error: 0,
-            quantity: total,
-            message: message,
-            decimos: decimos,
-          };
-        } else {
-          data = getDataPending();
+  // if (inTime(req.query.type)) {
+  let total = 0;
+  let status;
+  let statusElPais;
+  for (let decimo of decimos) {
+    let response = null;
+    try {
+      response = await checkOne(decimo.number, req.query.type);
+      if (response) {
+        statusElPais = response.status;
+        status = getStatus(statusElPais, response.total, response.timestamp);
+        const premio = getQuantityByAmount(decimo.amount, response.total);
+        if (status === "pending") {
+          break;
+        } else if (status === "win") {
+          total += premio;
         }
-      } catch (e) {
-        console.log("error333333333333", JSON.stringify(e));
+        decimo.status = status;
+        decimo.statusElPais = statusElPais;
+        decimo.quantity = premio;
+        if (status !== "pending") {
+          status = total > 0 ? "win" : "lost";
+        }
+        const message = getMessage(status, total, statusElPais);
+        data = {
+          error: 0,
+          quantity: total,
+          message: message,
+          decimos: decimos,
+        };
+      } else {
+        data = getDataPending();
       }
+    } catch (e) {
+      console.log("error333333333333", JSON.stringify(e));
     }
-  } else {
-    data = getDataPending();
   }
+  // } else {
+  //   data = getDataPending();
+  // }
 
   res.json(data);
 });
